47d93ee8db3a4b265100ab522dba967351f82a90
Timothy Madden <terminatorul@gmail.com>
2018-09-06 23:52:06 +0300

    Fixed cmake path for automatic tests.

--- tests/shellname_cmd_with_spaces.cmake
+++ tests/shellname_cmd_with_spaces.cmake
@@ -1,6 +1,6 @@
-#!"C:\Program Files\CMake\bin\cmake.exe" "-DSTRING_VARIABLE=Configuration 2" -P
+#!"C:\Program Files (x86)\CMake\bin\cmake.exe" "-DSTRING_VARIABLE=Configuration 2" -P
 
-cmake_minimum_required(VERSION 3.10)
+cmake_minimum_required(VERSION 3.12.1)
 
 if(NOT DEFINED STRING_VARIABLE OR NOT STRING_VARIABLE STREQUAL "Configuration 2")
     message(FATAL_ERROR "No variable received on the command line")
