67242849e014a1e8149ec106b91409c831ca9914
Timothy Madden <terminatorul@gmail.com>
2020-08-17 02:11:07 +0300

    Add option for "use v6" content marker line.
    Add command line options
        --scan-line
        --scan-line-count
        --specific-cmd
        --specific-cmd-arg
    
    to support scanning the beginning of the file for a content marker line,
    and running the associated interepreter command if found. Option is only
    used when there is no shebang line in the script.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 [![Build status](https://ci.appveyor.com/api/projects/status/q2s48eeh295xnksm/branch/master?svg=true)](https://ci.appveyor.com/project/terminatorul/win-shebang-line/branch/master)
 
 # Win-ScriptInvoke
-Windows file type handler for scripts with `#!` shebang line.
+Windows file type handler for scripts with `#!` shebang line or other content marker line at the begining of the file.
 
 ## Linux shebang line
 The shebang line (sharp-bang, that is `#` and `!`) is used as the first line of scripts on UNIX/Linux systems, to provide the path of the interpreter to run the script.
@@ -22,15 +22,18 @@ On Windows the interpreter for a script file is provided by its file association
 ## Same extension with different interpreters
 Using a file extension as its type means all files of a given extension will be run with the same interpreter. This is rather intuitive for any user and most of the time it works nice.
 
-But it is possible to have the same extension for files with different types. In my case I wanned to run perl scripts written with either [Perl 6](https://perl6.org) or [Perl 5](http://www.perl.org/), and use the same file extension `.pl` for both of them. A similar situation exists for [Python 3](https://docs.python.org/3/) and [Python 2](https://docs.python.org/2/) scripts. The two versions are not compatible with each other and they must use different interpreters.
+But it is possible to have the same extension for files with different types. In my case I wanned to run perl scripts written with either [Perl 6](https://www.raku.org) (now called Raku) or [Perl 5](http://www.perl.org/), and use the same file extension `.pl` for both of them. A similar situation exists for [Python 3](https://docs.python.org/3/) and [Python 2](https://docs.python.org/2/) scripts. The two versions are not compatible with each other and they must use different interpreters.
 
 In this case the shebang line can select the appropriate interpreter, while the file extension is no longer sufficient.
+
+## Content marker line
+Scripts like [Perl 6](https://www.raku.org) can also be identified by a specific line, reading for example `use v6;`, somewhere in the begining of the script file. So this file type handler can scan the first lines of the script for this purpose.
 
 ## Installation
 To install the file type handler you should:
 - choose a system-wide installation directory and clone or copy the script there. I used `C:\Local\bin\Win-ScriptInvoke.cmd`.
 - choose the file extension, for example `.pl` for perl scripts, and a name for the file type, like `PerlFamilyScript`.
-- remember to check that the file extension appears in `PATHEXT` environment variable. Append the extension, separated with `;`, if needed, like: `setx PATHEXT "%PATHEXT%;.pl"`.
+- remember to check that the file extension appears in `PATHEXT` environment variable. Append the extension, separated with `;`, if needed, like: `SetX PATHEXT "%PATHEXT%;.pl"`.
 - start a `cmd` window as Administrator and run the below commands:
 ```
     Assoc .pl=PerlFamilyScript
@@ -45,7 +48,7 @@ or just
 scriptName
 ```
 
-Windows may prompt you on the first use of the modified file type, to confirm file type change, so select the `Win-ScriptInvoke` handler then.
+in case you have the `PATHEXT` variable configured. Windows may prompt you on the first use of the modified file type, to confirm file type change, so select the `Win-ScriptInvoke` handler then.
 
 The file type handler will read the shebang line from the script and use the given interpreter to run it. A number of command-line options are available _for the file type handler_:
 ```
@@ -73,6 +76,22 @@ Options are:
            is --strip-extension, although it is recommended that your script files not include
            the .exe/.bat/... file extension on the shebang line.
 
+   --scan-line "line text"
+           Scan the first 10 lines of the script for a line that reads "line text", and if found
+           use the interpreter given with --spcific-cmd and --specific-cmd-arg
+
+   --scan-line-count "25"
+           Gives the number of initial lines in the script that will be scanned to look for the
+           "line text" given with --scan-line. Default is to scan initial 50 lines, to account
+           for licensing headers that sometimes clutter source code files.
+
+   --specific-cmd "specificCmd.exe"
+           Use "specificCmd.exe" to runt the script, if a line was found in the beginning of the
+           script that reads like the text given with --scan-line
+
+   --specific-cmd-arg "arg"
+           multiple times to pass multiple arguments to specificCmd.
+
    --default-cmd "cmdName.exe"
            Use cmdName.exe to run the script if the #! line is not found at the begining of
            the scripit file.
@@ -89,7 +108,7 @@ Options are:
 ```
 
 ## Usage
-You can use the file type handler with any scripts that provide a shebang line. If the line is not found at the begining of the script, then:
+You can use the file type handler with any scripts that provide a shebang line or optionally a content marker line. If neither is not found at the begining of the script, then:
 - if `--default-cmd` option is given, that command is invoked as the interpreter. Interpreter arguments given with `--default-cmd-arg` are included on the new command line.
 - if there was no `--default-cmd` option given, then an error is show like the following:
 ```
@@ -106,6 +125,7 @@ The way the file type handler checks the interpreter from shebang line is:
 - if no path component is given for the interpreter name, search the base file name on `PATH`. For each of the paths:
     * append every extension from `PATHEXT` in turn and see if the resulting name can be found. These checks allow you to use a shebang line like `#!perl`, and then `perl.exe` can be found (same process used by `cmd.exe`).
 - if above steps fail then keep the base name of the interpreter and remove (ignore) the path. This is meant to support Linux paths like `/usr/bin/perl6` that will not be found on Windows, but the base name `perl6` may still be found (on `PATH`). Also remove the extension from the base name. That is, an interpreter given as `perl6.exe` can be replaced with `perl6.bat` if the later is found on `PATH`. This behavior can be disabled with `--no-strip-location` and `--no-strip-extension` options.
+- if there is no shebang line at the start of the file, the first lines are then searched for a content marker line, if one was provided. If a content marker like is found, the associated interpreter and interpreter args are invoked, to run the script with the script args.
 - if all checks fail then use the default command if it was given with `--default-cmd` option
 - without `--default-cmd`, display a message like 
   `scriptName.cmd: Bad interpreter: <interpreter name>`
@@ -113,4 +133,4 @@ The way the file type handler checks the interpreter from shebang line is:
 
 As it is a `.cmd` script, the file type handler will open a console window in order to run the script. This is an issue if the script is a graphical (GUI) application. For this case a non-console version of the file type handler will be needed, that is _not_ provided here (maybe later).
 
-Also note that file type handlers on Windows can be more complex and based on registering shell-specific .dll functions, so this script is a simple approach to the issue.
+Also note that file type handlers on Windows can be more complex, based on registering shell-specific .dll functions, so this script is a simple approach to the issue.
--- Win-ScriptInvoke.cmd
+++ Win-ScriptInvoke.cmd
@@ -1,5 +1,6 @@
 @Echo Off
 
+SetLocal
 SetLocal EnableDelayedExpansion
 
 GoTo :ParseCommandLine
@@ -27,6 +28,23 @@ Echo            The extension part of the #^^!-command is ignored if given but n
 Echo            executable found with the same name is invoked regardless of its extension. The>&2
 Echo            default is --strip-extension, although it is recommended that your script files>&2
 Echo            not include the .exe/.bat file extension on the shebang line.>&2
+Echo.>&2
+Echo    --scan-line "line text">&2
+Echo            Scan the first 10 lines of the script for a line that reads "line text", and if found>&2
+Echo            use the interpreter given with --spcific-cmd and --specific-cmd-arg>&2
+Echo.>&2
+Echo    --scan-line-count "25">&2
+Echo            Gives the number of initial lines in the script that will be scanned to look for the>&2
+Echo            "line text" given with --scan-line. Default is to scan initial 50 lines., to account>&2
+Echo            for licensing headers that sometimes clutter source code files.>&2
+Echo.>&2
+Echo    --specific-cmd "specificCmd.exe">&2
+Echo            Use "specificCmd.exe" to runt the script, if a line was found in the beginning of the>&2
+Echo            script that reads like the text given with --scan-line>&2
+Echo.>&2
+Echo    --specific-cmd-arg "arg">&2
+Echo            Use given arg on the command line when running "specificCmd.exe". Use the option>%2
+Echo            multiple times to pass multiple arguments to specificCmd.>&2
 Echo.>&2
 Echo    --default-cmd "cmdName.exe">&2
 Echo            Use cmdName.exe to run the script if the #^^! line is not found at the begining of>&2
@@ -57,11 +75,12 @@ Set SCRIPT_INVOKE_STRIP_EXTENSION=1
 Set SCRIPT_INVOKE_DEFAULT_CMD=
 Set SCRIPT_INVOKE_DEFAULT_CMD_ARGS=
 Set SCRIPT_INVOKE_FALLBACK_TO_DEFAULT=1
+Set SCRIPT_INVOKE_SCAN_LINE_COUNT=50
 
 :ParseCommandLineOption
-If /I "%~1" == "/?"	 GoTo :ShowSyntax
-If /I "%~1" == "-h"	 GoTo :ShowSyntax
-If /I "%~1" == "--help"	 GoTo :ShowSyntax
+If /I "%~1" == "/?"      GoTo :ShowSyntax
+If /I "%~1" == "-h"      GoTo :ShowSyntax
+If /I "%~1" == "--help"  GoTo :ShowSyntax
 If /I "%~1" == "--usage" GoTo :ShowSyntax
 
 If /I "%~1" == "--debug" (
@@ -111,6 +130,30 @@ If /I "%~1" == "--no-fallback-to-default" (
     GoTo :NextCommandArg
 )
 
+If /I "%~1" == "--scan-line" (
+    Set SCRIPT_INVOKE_SCAN_LINE=%~2
+    Shift
+    GoTo :NextCommandArg
+)
+
+If /I "%~1" == "--scan-line-count" (
+    Set SCRIPT_INVOKE_SCAN_LINE_COUNT=%~2
+    Shift
+    GoTo :NextCommandArg
+)
+
+If /I "%~1" == "--specific-cmd" (
+    Set SCRIPT_INVOKE_SPECIFIC_INTERPRETER=%2
+    Shift
+    GoTo :NextCommandArg
+)
+
+If /I "%~1" == "--specific-cmd-arg" (
+    Set SCRIPT_INVOKE_SPECIFIC_CMD_ARGS=!SCRIPT_INVOKE_SPECIFIC_CMD_ARGS! %2
+    Shift
+    GoTo :NextCommandArg
+)
+
 If "%~1" == "--" (
     Shift
     GoTo :ParseScriptCommand
@@ -136,145 +179,174 @@ If Not x%1 == x (
 )
 
 If Defined SCRIPT_INVOKER_DEBUG (
-    Echo Strip location: !SCRIPT_INVOKE_STRIP_LOCATION!
+    Echo Strip location:  !SCRIPT_INVOKE_STRIP_LOCATION!
     Echo Strip extension: !SCRIPT_INVOKE_STRIP_EXTENSION!
+    Echo Scan line:       !SCRIPT_INVOKE_SCAN_LINE!
+    Echo Scan line count: !SCRIPT_INVOKE_SCAN_LINE_COUNT!
+    Echo Scan line cmd:   !SCRIPT_INVOKE_SPECIFIC_INTERPRETER! !SCRIPT_INVOKE_SPECIFIC_CMD_ARGS!
     Echo Default command: !SCRIPT_INVOKE_DEFAULT_CMD! !SCRIPT_INVOKE_DEFAULT_CMD_ARGS!
     Echo Fallback to cmd: !SCRIPT_INVOKE_FALLBACK_TO_DEFAULT!
-    Echo Script command line: !SCRIPT_COMMAND_LINE!
+    Echo Script cmd line: !SCRIPT_COMMAND_LINE!
 )
 
+:ReadShebangLine
 For /F "usebackq tokens=1,* delims=!" %%i In (!SCRIPT_FILE_NAME!) Do (
     Rem Echo %%i %%j
 
     Rem Check the first line begins with #!
     If x"%%i" == x"#" (
-	If Not x"%%j" == x"" (
-	    Rem Echo "Found shebang line: %%i^!%%j"
+        If Not x"%%j" == x"" (
+            Rem Echo "Found shebang line: %%i^!%%j"
 
-	    Call :ParseShebangLine %%j
+            Call :ParseShebangLine %%j
 
-	    If Defined SCRIPT_INVOKER_DEBUG (
-		Echo Interpreter: !INTERPRETER!
+            If Defined SCRIPT_INVOKER_DEBUG (
+                Echo Interpreter: !INTERPRETER!
                 Echo Args:        !INTERPRETER_ARGS!
-	    )
+            )
 
-	    Rem Check for a built-in cmd command
-	    For /F "usebackq delims=`" %%l In (`help`) Do (
-		Set LINE=%%l
-		If Not "!LINE:~0,1!" == " " (
-		    For %%y in (!INTERPRETER!) Do (
-			For /F %%c In ("!LINE!") Do (
-			    If /I "%%c" == "%%~y" (
-				Set INTERPRETER=%%~y
-				GoTo :RunInterpreterWithArgs
-			    )
-			)
-		    )
-		)
-	    )
+            Rem Check for a built-in cmd command
+            For /F "usebackq delims=`" %%l In (`help`) Do (
+                Set LINE=%%l
+                If Not "!LINE:~0,1!" == " " (
+                    For %%y in (!INTERPRETER!) Do (
+                        For /F %%c In ("!LINE!") Do (
+                            If /I "%%c" == "%%~y" (
+                                Set INTERPRETER=%%~y
+                                GoTo :RunInterpreterWithArgs
+                            )
+                        )
+                    )
+                )
+            )
 
-	    for %%n In ("/bin/env" "/usr/bin/env") Do (
-		If "%%~n" == "!INTERPRETER_FILE_NAME!" Call :ParseShebangLine !INTERPRETER_ARGS!
-	    )
+            for %%n In ("/bin/env" "/usr/bin/env") Do (
+                If "%%~n" == "!INTERPRETER_FILE_NAME!" Call :ParseShebangLine !INTERPRETER_ARGS!
+            )
 
-	    Set INTERPR=!INTERPRETER!
+            Set INTERPR=!INTERPRETER!
 
-	    If Defined SCRIPT_INVOKE_STRIP_EXTENSION (
-		For %%k In (!INTERPRETER!) Do (
-		    If Not x"%%~xk" == x"" (
-			Call :TrimFileExtension !INTERPRETER!
-			Set INTERPR=!INTERPR! "!TRIMMED!"
-		    )
-		)
-	    )
+            If Defined SCRIPT_INVOKE_STRIP_EXTENSION (
+                For %%k In (!INTERPRETER!) Do (
+                    If Not x"%%~xk" == x"" (
+                        Call :TrimFileExtension !INTERPRETER!
+                        Set INTERPR=!INTERPR! "!TRIMMED!"
+                    )
+                )
+            )
 
-	    If Defined SCRIPT_INVOKER_DEBUG (
-		Echo INTERPR: !INTERPR!
-	    )
+            If Defined SCRIPT_INVOKER_DEBUG (
+                Echo INTERPR: !INTERPR!
+            )
 
-	    If "!INTERPRETER_FILE_NAME:\=;!" == "!INTERPRETER_FILE_NAME!" (
-		If "!INTERPRETER_FILE_NAME::=;!" == "!INTERPRETER_FILE_NAME!" (
-		    Rem No path to interpreter, only name given, like: #!cmake
-		    For %%w In (!INTERPR!) Do (
-			For %%y In ("." "%PATH:;=" "%") Do (
-			    For %%x In ("" "%PATHEXT:;=" "%") Do (
-				If Exist "%%~y\%%~w%%~x" (
-				    Set INTERPRETER="%%~y\%%~w%%~x"
-				    GoTo :RunInterpreterWithArgs
-				)
-			    )
-			)
-		    )
-		)
+            If "!INTERPRETER_FILE_NAME:\=;!" == "!INTERPRETER_FILE_NAME!" (
+                If "!INTERPRETER_FILE_NAME::=;!" == "!INTERPRETER_FILE_NAME!" (
+                    Rem No path to interpreter, only name given, like: #!cmake
+                    For %%w In (!INTERPR!) Do (
+                        For %%y In ("." "%PATH:;=" "%") Do (
+                            For %%x In ("" "%PATHEXT:;=" "%") Do (
+                                If Exist "%%~y\%%~w%%~x" (
+                                    Set INTERPRETER="%%~y\%%~w%%~x"
+                                    GoTo :RunInterpreterWithArgs
+                                )
+                            )
+                        )
+                    )
+                )
 
-		GoTo :StripLocation
-	    )
-	    
-	    Rem Some path to interpreter given, like #!\Local\bin\sed or #!C:sed.exe
-	    Rem Skip PATH search
-	    For %%w In (!INTERPR!) Do (
-		For %%x In ("" "%PATHEXT:;=" "%") Do (
-		    If Exist "%%~w%%~x" (
-			Set INTERPRETER="%%~w%%~x"
-			GoTo :RunInterpreterWithArgs
-		    )
-		)
-	    )
+                GoTo :StripLocation
+            )
+
+            Rem Some path to interpreter given, like #!\Local\bin\sed or #!C:sed.exe
+            Rem Skip PATH search
+            For %%w In (!INTERPR!) Do (
+                For %%x In ("" "%PATHEXT:;=" "%") Do (
+                    If Exist "%%~w%%~x" (
+                        Set INTERPRETER="%%~w%%~x"
+                        GoTo :RunInterpreterWithArgs
+                    )
+                )
+            )
 
 :StripLocation
-	    If Defined SCRIPT_INVOKE_STRIP_LOCATION (
-		Call :GetBaseName !INTERPRETER!
+            If Defined SCRIPT_INVOKE_STRIP_LOCATION (
+                Call :GetBaseName !INTERPRETER!
 
-		For %%y In (!INTERPRETER!) Do (
-		    If /I Not "!BASENAME!" == "%%~y" (
-			Set INTERPR="!BASENAME!"
+                For %%y In (!INTERPRETER!) Do (
+                    If /I Not "!BASENAME!" == "%%~y" (
+                        Set INTERPR="!BASENAME!"
 
-			If Defined SCRIPT_INVOKE_STRIP_EXTENSION (
-			    For %%k In ("!BASENAME!") Do (
-				If Not x"%%~xk" == x"" (
-				    Call :TrimFileExtension "!BASENAME!"
-				    Set INTERPR=!INTERPR! "!TRIMMED!"
-				)
-			    )
-			)
+                        If Defined SCRIPT_INVOKE_STRIP_EXTENSION (
+                            For %%k In ("!BASENAME!") Do (
+                                If Not x"%%~xk" == x"" (
+                                    Call :TrimFileExtension "!BASENAME!"
+                                    Set INTERPR=!INTERPR! "!TRIMMED!"
+                                )
+                            )
+                        )
 
-			For %%w In (!INTERPR!) Do (
-			    For %%k In ("." "%PATH:;=" "%") Do (
-				For %%l In ("" "%PATHEXT:;=" "%") Do (
-				    If Exist "%%~k\%%~w%%~l" (
-					Set INTERPRETER="%%~k\%%~w%%~l"
-					GoTo :RunInterpreterWithArgs
-				    )
-				)
-			    )
-			)
-		    )
-		)
-	    )
+                        For %%w In (!INTERPR!) Do (
+                            For %%k In ("." "%PATH:;=" "%") Do (
+                                For %%l In ("" "%PATHEXT:;=" "%") Do (
+                                    If Exist "%%~k\%%~w%%~l" (
+                                        Set INTERPRETER="%%~k\%%~w%%~l"
+                                        GoTo :RunInterpreterWithArgs
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
 
-	    Rem #!-command was not found in any way
-	    If Defined SCRIPT_INVOKE_FALLBACK_TO_DEFAULT (
-		If Defined SCRIPT_INVOKE_DEFAULT_CMD (
-		    Set INTERPRETER=!SCRIPT_INVOKE_DEFAULT_CMD!
-		    Set INTERPRETER_ARGS=!SCRIPT_INVOKE_DEFAULT_CMD_ARGS!
-		    GoTo :RunInterpreterWithArgs
-		)
-	    )
+            Rem #!-command was not found in any way
+            If Defined SCRIPT_INVOKE_FALLBACK_TO_DEFAULT (
+                If Defined SCRIPT_INVOKE_DEFAULT_CMD (
+                    Set INTERPRETER=!SCRIPT_INVOKE_DEFAULT_CMD!
+                    Set INTERPRETER_ARGS=!SCRIPT_INVOKE_DEFAULT_CMD_ARGS!
+                    GoTo :RunInterpreterWithArgs
+                )
+            )
 
-	    Rem Invalid #!-command interpreter, will trigger an error on invocation
-	    Echo !SCRIPT_FILE_NAME!: Bad interpreter !INTERPRETER! >&2
-	    GoTo :RunInterpreterWithArgs
-	)
+            Rem Invalid #!-command interpreter, will trigger an error on invocation
+            Echo !SCRIPT_FILE_NAME!: Bad interpreter !INTERPRETER! >&2
+            GoTo :RunInterpreterWithArgs
+        )
     )
 
     Rem No shebang line
-    If Defined SCRIPT_INVOKE_DEFAULT_CMD (
-	Set INTERPRETER=!SCRIPT_INVOKE_DEFAULT_CMD!
-	Set INTERPRETER_ARGS=!SCRIPT_INVOKE_DEFAULT_CMD_ARGS!
-	GoTo :RunInterpreterWithArgs
+
+    If Defined SCRIPT_INVOKE_SCAN_LINE (
+        Set SCRIPT_INVOKE_SCRIPT_LINE_NUMBER=0
+        For /F "usebackq tokens=1*" %%k In (!SCRIPT_FILE_NAME!) Do (
+            Set /A SCRIPT_INVOKE_SCRIPT_LINE_NUMBER=!SCRIPT_INVOKE_SCRIPT_LINE_NUMBER! + 1
+
+            If !SCRIPT_INVOKE_SCRIPT_LINE_NUMBER! GTR !SCRIPT_INVOKE_SCAN_LINE_COUNT! (
+                GoTo :ReadShebangLine
+            )
+
+            If "%%k %%l" == "%SCRIPT_INVOKE_SCAN_LINE%" (
+                If Defined SCRIPT_INVOKER_DEBUG (
+                    Echo Found scan line: %%k %%l
+                )
+                Set INTERPRETER=!SCRIPT_INVOKE_SPECIFIC_INTERPRETER!
+                Set INTERPRETER_ARGS=!SCRIPT_INVOKE_SPECIFIC_CMD_ARGS!
+                GoTo :RunInterpreterWithArgs
+            )
+        )
     )
 
-    Echo Expected initial #^^! line with script interpreter name for !SCRIPT_FILE_NAME! >&2
+    If Defined SCRIPT_INVOKE_DEFAULT_CMD (
+        Set INTERPRETER=!SCRIPT_INVOKE_DEFAULT_CMD!
+        Set INTERPRETER_ARGS=!SCRIPT_INVOKE_DEFAULT_CMD_ARGS!
+        GoTo :RunInterpreterWithArgs
+    )
+
+    If Defined SCRIPT_INVOKE_SCAN_LINE (
+        Echo Expected initial #^^! line with script interpreter name, or content marker line "!SCRIPT_INVOKE_SCAN_LINE!", for !SCRIPT_FILE_NAME! >&2
+    ) Else (
+        Echo Expected initial #^^! line with script interpreter name for !SCRIPT_FILE_NAME! >&2
+    )
     Exit /B 255
 )
 
@@ -285,11 +357,11 @@ Set INTERPRETER_ARGS=
 
 For %%i in (%*) Do (
     If Defined INTERPRETER (
-	Set INTERPRETER_ARGS=!INTERPRETER_ARGS! %%i
+        Set INTERPRETER_ARGS=!INTERPRETER_ARGS! %%i
     ) Else (
-	Set INTERPRETER=%%i
-	Set INTERPRETER=!INTERPRETER:/=\!
-	Set INTERPRETER_FILE_NAME=%%~i
+        Set INTERPRETER=%%i
+        Set INTERPRETER=!INTERPRETER:/=\!
+        Set INTERPRETER_FILE_NAME=%%~i
     )
 )
 GoTo :EOF
